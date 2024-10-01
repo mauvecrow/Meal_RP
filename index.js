934812b0e0859da8996a31c1db2acc21f1a9c540
Bradley Quangson <bquangson@gmail.com>
2024-09-30 21:08:17 -0600

    update to include Mongoose. Connect to Mongo Atlas, url in .env

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
 
+const dbUrl = process.env.DB_URL;
+
+mongoose.connect(dbUrl)
+    .then( (result) => {
+        console.log("Database connected");
+        app.listen(port, () => {
+            console.log("Listening on port: " + port);
+        })
+    })
+    .catch( err => console.log(err));
+
 app.get("/", (req, res) => {
     res.send("Welcome to Meal ERP")
-})
-
-app.listen(port, () => {
-    console.log("Listening on port: " + port);
 })
